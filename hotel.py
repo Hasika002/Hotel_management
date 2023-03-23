00e1dfa1520136e119ce10bb902158820e0420ed
himanshu <himanshu123@gmail.com>
2023-03-23 16:01:32 +0530

    himanshu contri

--- hotel.py
+++ hotel.py
@@ -134,3 +134,64 @@ if (choice=="checkin"):
                 print(bill)
             else:
                 pass
+                else:
+                sum_total=0
+                print("                 M_E_N_U_E                       ")
+                print("""           ### CHINESE ###
+
+    Hakka Noodles          हक्का नूडल्स       120/-          1
+
+    Veg Choumin            वेज चाउमिन        110/-          2
+
+    Veg Noodles            वेज नूडल्स          110/-          3
+
+    Veg Chopsuey           वेज चोपसी         140/-           4   
+
+    Veg Fried Rice         वेज फ्राइड राइस     105/-           5
+
+    Veg Kofte              वेज कोथे           125/-           6
+
+    Veg Manchurian Dry     वेज मंचूरियन ड्राय     140/-           7
+
+    Veg Manchurian GREAVY  वेज मंचूरियन ग्रेवी     120/-            8
+
+    Chilli Paneer Dry      चिल्ली पनीर ड्राय      140/-           9
+
+    Chilli Paneer GREAVY   चिल्ली पनीर ग्रेवी      140/-            10
+
+    Paneer Manchurian      पनीर मंचूरियन       135/-             11
+
+    Chinese Bhel          चायनीस गेल         135/-              12
+
+    Gobhi Chili            गोभी चिल्ली         140/-              13
+
+    Gobhi Manchurian       गोभी मंचूरियन       125/-              14
+
+    Paneer Tikka Dry       पनीर टिक्का ड्राय     125/-              15
+
+    Manchurian Noodles     मंचूरियन नूडल्स      175/-              16
+
+    Veg Sizler             वेज सिज़्लर         175/-               17
+
+    Chana chili Dry        चना चिल्ली ड्राय      125/-               18
+
+    Hani Batato            हनी बाते           240/-               19
+
+    Veg MOMOSs             वेज मोमोस         130/-               20
+
+    *** TO EXIT PRESS 900 ***
+    """)
+            n_a=[1,3,7,8,19]
+            chinease={1:120, 2:110, 3:110, 4:140, 5:105, 6:125, 7:140, 8:120, 9:140, 10:140, 11:135, 12:135, 13:140, 14:125, 15:125, 16:175, 17:175, 18:125, 19:240, 20:130}
+            while (choice!=0):
+                order=int(input("ENTER THE FOOD CODE YOU WANT TO ORDER: "))
+                if order in n_a:
+                    print("SORRY THE ITEM IS UNAVALIBLE")
+                elif order==900:
+                     break
+                elif (order>len(chinease)):
+                    print("PLEASE ENTER FROM THE MENUE")    
+                else:
+                    qat=int(input("ENTER THE NO. OF ORDERS: "))
+                    sum_total=sum_total+(chinease[order]*qat)
+                    print("YOUR BILL IS:",sum_total)
